5efbecc4685aece08a2255cef1133b6625f3d180
Sujal Patil <151530482+Sujal1201@users.noreply.github.com>
2024-09-05 15:08:12 +0530

    sujal.js

---
+++ index.js
@@ -0,0 +1,73 @@
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('[data-cell]');
+const restartButton = document.getElementById('restartButton');
+let isXTurn = true;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function startGame() {
+    isXTurn = true;
+    cells.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('o');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = isXTurn ? 'x' : 'o';
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    isXTurn = !isXTurn;
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains('x') || cell.classList.contains('o');
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        alert("It's a Draw!");
+    } else {
+        alert(`${isXTurn ? "X" : "O"} Wins!`);
+    }
+    startGame();
+}
+
+restartButton.addEventListener('click', startGame);
+
+startGame();
